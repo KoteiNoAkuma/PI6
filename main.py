4c495f55c9d84ddd7ac1d861110562e41ea3efff
kirayne <83626104+kirayne@users.noreply.github.com>
2023-11-22 20:09:29 -0300

    Update main.py
    Todos os algoritmos predizem o dia seguinte

--- main.py
+++ main.py
@@ -7,19 +7,17 @@ import datetime
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.neural_network import MLPRegressor
 from sklearn.metrics import r2_score
 from sklearn.linear_model import LinearRegression
-import tensorflow as tf
-from keras.models import Sequential
-from keras.layers import LSTM, SimpleRNN, Dense
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import SVR
+
 
 data = None
 interval = "1d"
 limit = 1000
+window_size = 60
 
 
 def get_binance_datarequest(ticker, interval, limit, start='2022-03-01 00:00:00'):
@@ -34,92 +32,135 @@ def get_binance_datarequest(ticker, interval, limit, start='2022-03-01 00:00:00'
 
 get_binance_datarequest('BTCUSDT', interval, limit)
 
-# Calculando RSI usando TA-Lib
-def calculateRSI(data):
+# Calculando RSI usando TA-Lib 
+def calculateRSI(data): # Não mais usado
     return talib.RSI(np.array(data['close']), timeperiod=14)
 
-def calculatePricing(data):
+def calculatePricing(data): 
     return data['close']
 
-def calculateVolume(data):
+def calculateVolume(data): # Não mais usado
     return data['volume']
 
 # Calculo do RSI
 rsi_values = calculateRSI(data)
 
 RSI = pd.DataFrame({'RSI': rsi_values}, index=data.index[-len(rsi_values):]) # Dessa forma esse dataframe vai ter o mesmo tamanho dos outros dois dataframes
-PRICING = calculatePricing(data)
+price = calculatePricing(data)
 VOLUME = calculateVolume(data)
 
-history_df = pd.concat([RSI, VOLUME, PRICING], axis=1) # Junta os 3 dataframes em um Único
-history_df = history_df.dropna() # Remove as linhas com NaN do dataframe
+history_df = pd.concat([RSI, VOLUME, price], axis=1) # Junta os 3 dataframes em um Único
+price = price.dropna().values # Remove as linhas com NaN do dataframe
 
 # Preparando dados para treinamento e teste
-X = history_df[["RSI", "volume"]].values
-y = history_df['close'].values
+X = []
+y = []
+
+# Janelamento
+for i in range(len(price) - window_size):
+    window = price[i:i+window_size] # Elemento 61 nao entra no primeiro loop
+    target = price[i+window_size] #  Elemento 60 + i
+
+    X.append(window)
+    y.append(target)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42) # 80% treinamento, 20% teste
-
 
 # Random Forest
 def randomForestRegression(X_train, X_test, y_train, y_test):
     rf_regressor = RandomForestRegressor(n_estimators=100, random_state=0)
-    rf_regressor.fit(X_train, y_train)
-    y_pred = rf_regressor.predict(X_test)
-    r2_rf = r2_score(y_test, y_pred)
+    rf_regressor.fit(X, y)
+    y_pred = rf_regressor.predict(X)
+    preco_previsto = y_pred[-1]
+
+    r2_rf = r2_score(y, y_pred)
+    print('=========== Resultados ===========')
     print("Real Data: ")
-    print(y_test)
-    print('===========')
-    print("Predicted Data: ")
+    print(y)
+    print('=========== Previsões ===========')
     print(y_pred)
-    print('===========')
+    print('=========== Preço Previsto ===========')
+    print("Preço previsto para o próximo dia:", preco_previsto)
+    print('=========== R² ===========')
+    print("R² Score:", r2_rf)
 
     return y_pred, r2_rf
 
 
 
 y_pred_rf, r2_rf = randomForestRegression(X_train, X_test, y_train, y_test)
-print("Random Forest R2: ", r2_rf)
+
+print("Random Forest R2: ", r2_rf) 
 
 # Regressao linear
 def linear_regression(X_train, X_test, y_train, y_test):
     regr = LinearRegression()
-    regr.fit(X_train, y_train)
-    y_pred = regr.predict(X_test)
-    r2 = r2_score(y_test, y_pred)
+    regr.fit(X, y)
+    y_pred = regr.predict(X)
+    preco_previsto = y_pred[-1]
+
+    r2 = r2_score(y, y_pred)
+    print('=========== Resultados ===========')
     print("Real Data: ")
-    print(y_test)
-    print('===========')
-    print("Predicted Data: ")
+    print(y)
+    print('=========== Previsões ===========')
     print(y_pred)
-    print('===========')
+    print('=========== Preço Previsto ===========')
+    print("Preço previsto para o próximo dia:", preco_previsto)
+    print('=========== R² ===========')
+    print("R² Score:", r2)
+
 
     return y_pred, r2
 
 y_pred, result = linear_regression(X_train, X_test, y_train, y_test)
+print("Linear Regression R2: ", result) 
+ 
+# Knn Regression
+def knn_regression(X, y, n_neighbors=5):
+    knn_regressor = KNeighborsRegressor(n_neighbors=n_neighbors)
+    knn_regressor.fit(X, y)
+    y_pred = knn_regressor.predict(X_test)  # Altere esta linha para usar X_test ao invés de X
 
-print("Linear Regression R2: ", result)
+    preco_previsto = y_pred[-1]
 
-# print(history_df) # Retorna dados de 16 de março de 2022 até 2 de novembro de 2023  597 dias  (Com algumas lacunas)
+    r2 = r2_score(y_test, y_pred)  # Use y_test em vez de y
 
-# # MLP 
-# def mlp(X_train, X_test, y_train, y_test):
-#     regr = MLPRegressor(random_state=1, max_iter=500)
-#     regr.fit(X_train, y_train)
-#     y_pred = regr.predict(X_test)
-#     r2 = r2_score(y_test, y_pred)
+    print('=========== Resultados ===========')
+    print("Real Data: ")
+    print(y_test)
+    print('=========== Previsões ===========')
+    print(y_pred)
+    print('=========== Preço Previsto ===========')
+    print("Preço previsto para o próximo dia:", preco_previsto)
+    print('=========== R² ===========')
+    print("R² Score:", r2)
 
-#     print(y_test)
-#     print('===========')
-#     print(y_pred)
+    return y_pred, r2
 
-#     print('===========')
+y_pred_knn, r2_knn = knn_regression(X_train, y_train)  # Ajuste a chamada da função com apenas dois argumentos
 
-#     return y_pred, r2
-# y_pred, result = mlp(X_train, X_test, y_train, y_test)
+# SVR Regression
+def svr_regression(X, y):
+    svr_regressor = SVR(kernel='rbf') 
+    svr_regressor.fit(X, y)
+    y_pred = svr_regressor.predict(X_test)
 
-# print(result)
+    preco_previsto = y_pred[-1]
 
+    r2 = r2_score(y_test, y_pred)
 
+    print('=========== Resultados ===========')
+    print("Real Data: ")
+    print(y_test)
+    print('=========== Previsões ===========')
+    print(y_pred)
+    print('=========== Preço Previsto ===========')
+    print("Preço previsto para o próximo dia:", preco_previsto)
+    print('=========== R² ===========')
+    print("R² Score:", r2)
 
+    return y_pred, r2
 
+y_pred_svr, r2_svr = svr_regression(X_train, y_train)  
+print("SVR R2: ", r2_svr)
