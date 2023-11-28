78486abf58c32039335b19fc657004ae35162573
kirayne <83626104+kirayne@users.noreply.github.com>
2023-11-27 23:14:36 -0300

    Update main.py
    final

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 import datetime
 import pandas as pd
 import numpy as np
+import threading
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 from sklearn.linear_model import LinearRegression
@@ -11,6 +12,8 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.svm import SVR
 from sklearn.metrics import mean_absolute_error
+from tkinter import *
+import tkinter.font as tkFont
 
 def get_binance_datarequest(ticker, interval, limit, start='2022-03-01 00:00:00'):
     columns = ['open_time','open', 'high', 'low', 'close', 'volume','close_time', 'qav','num_trades','taker_base_vol','taker_quote_vol', 'ignore']
@@ -23,23 +26,16 @@ def get_binance_datarequest(ticker, interval, limit, start='2022-03-01 00:00:00'
 
     return data
 
-def calculateRSI(data):
-    return talib.RSI(np.array(data['close']), timeperiod=14)
-
 def calculatePricing(data): 
     return data['close']
 
-def calculateVolume(data):
-    return data['volume']
 
 def prepare_data(data, window_size=60):
-    rsi_values = calculateRSI(data)
-    RSI = pd.DataFrame({'RSI': rsi_values}, index=data.index[-len(rsi_values):])
     price = calculatePricing(data)
-    VOLUME = calculateVolume(data)
-
-    history_df = pd.concat([RSI, VOLUME, price], axis=1)
     price = price.dropna().values
+    
+    previous_close = data['close'].iloc[-1]  # Valor de fechamento do dia atual
+
 
     X = []
     y = []
@@ -51,10 +47,10 @@ def prepare_data(data, window_size=60):
         X.append(window)
         y.append(target)
 
-    return X, y
+    return X, y, previous_close
 
 def randomForestRegression(X, y):
-    rf_regressor = RandomForestRegressor(n_estimators=100, random_state=0)
+    rf_regressor = RandomForestRegressor(n_estimators=200, random_state=0)
     rf_regressor.fit(X, y)
     y_pred = rf_regressor.predict(X)
     predicted_price = y_pred[-1]
@@ -67,7 +63,7 @@ def randomForestRegression(X, y):
     print('R² Score:', r2)
     print('Mean Absolute Error (MAE):', mae)
 
-    return y_pred, r2, mae
+    return y_pred, r2, mae, predicted_price
 
 def linear_regression(X, y):
     regr = LinearRegression()
@@ -83,7 +79,7 @@ def linear_regression(X, y):
     print('R² Score:', r2)
     print('Mean Absolute Error (MAE):', mae)
 
-    return y_pred, r2, mae
+    return y_pred, r2, mae, predicted_price
 
 def knn_regression(X, y, n_neighbors=5):
     knn_regressor = KNeighborsRegressor(n_neighbors=n_neighbors)
@@ -99,10 +95,10 @@ def knn_regression(X, y, n_neighbors=5):
     print('R² Score:', r2)
     print('Mean Absolute Error (MAE):', mae)
 
-    return y_pred, r2, mae
+    return y_pred, r2, mae, predicted_price
 
 def svr_regression(X, y):
-    svr_regressor = SVR(kernel='rbf') 
+    svr_regressor = SVR(kernel='rbf')
     svr_regressor.fit(X, y)
     y_pred = svr_regressor.predict(X)
     predicted_price = y_pred[-1]
@@ -115,9 +111,38 @@ def svr_regression(X, y):
     print('R² Score:', r2)
     print('Mean Absolute Error (MAE):', mae)
 
-    return y_pred, r2, mae
+    return y_pred, r2, mae, predicted_price
 
 # Para melhor organizaçao do codigo
+
+
+def design(predicted_rf, r2_rf, mae_rf, predicted_lr, r2_lr, mae_lr, predicted_knn, r2_knn, mae_knn, predicted_svr, r2_svr, mae_svr, previous_close):
+    top = Tk()
+    top.geometry("700x450")
+    fontExample = tkFont.Font(family="Arial", size=9, weight="bold", slant="italic")
+    algoritmo = Label(top, text="ALGORITMO", font=fontExample).place(x=30, y=50)
+    erro = Label(top, text="MAE", font=fontExample).place(x=180, y=50)
+    valorPrevisto = Label(top, text="VALOR PREVISTO", font=fontExample).place(x=280, y=50)
+    r2 = Label(top, text="R2", font=fontExample).place(x=465, y=50)
+    valorAtual = Label(top, text="VALOR ATUAL", font=fontExample).place(x=580, y=50)
+    randomForest = Label(top, text="Random\nForest").place(x=30, y=120)
+    linearRegression = Label(top, text="Linear\nRegression").place(x=30, y=180)
+    knnRegression = Label(top, text="KNN\nRegression").place(x=30, y=240)
+    svrRegression = Label(top, text="SVR\nRegression").place(x=30, y=300)
+    error1 = Label(top, text=str(round(mae_rf, 2))).place(x=180, y=120)
+    error2 = Label(top, text=str(round(mae_lr, 2))).place(x=180, y=180)
+    error3 = Label(top, text=str(round(mae_knn, 2))).place(x=180, y=240)
+    error4 = Label(top, text=str(round(mae_svr, 2))).place(x=180, y=300)
+    value_predict1 = Label(top, text=str(round(predicted_rf, 2))).place(x=310, y=120)
+    value_predict2 = Label(top, text=str(round(predicted_lr, 2))).place(x=310, y=180)
+    value_predict3 = Label(top, text=str(round(predicted_knn, 2))).place(x=310, y=240)
+    value_predict4 = Label(top, text=str(round(predicted_svr, 2))).place(x=310, y=300)
+    r2_Rf = Label(top, text=str(round(r2_rf, 2))).place(x=450, y=120)
+    r2_Lr = Label(top, text=str(round(r2_lr, 2))).place(x=450, y=180)
+    r2_Knn = Label(top, text=str(round(r2_knn, 2))).place(x=450, y=240)
+    r2_Svr = Label(top, text=str(round(r2_svr, 2))).place(x=450, y=300)
+    current_value = Label(top, text=str(round(previous_close, 2))).place(x=600, y=210)
+    top.mainloop()
 
 def run():
     ticker = 'BTCUSDT'
@@ -126,12 +151,13 @@ def run():
     window_size = 60
 
     data = get_binance_datarequest(ticker, interval, limit)
-    X, y = prepare_data(data, window_size)
+    X, y, previous_close = prepare_data(data, window_size)
 
-    y_pred_rf, r2_rf, mae_rf = randomForestRegression(X, y)
-    y_pred_lr, r2_lr, mae_lr = linear_regression(X, y)
-    y_pred_knn, r2_knn, mae_knn = knn_regression(X, y)
-    y_pred_svr, r2_svr, mae_svr = svr_regression(X, y)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-
+    y_pred_rf, r2_rf, mae_rf, predicted_rf = randomForestRegression(X_train, y_train)
+    y_pred_lr, r2_lr, mae_lr, predicted_lr = linear_regression(X_train, y_train)
+    y_pred_knn, r2_knn, mae_knn, predicted_knn = knn_regression(X_train, y_train)
+    y_pred_svr, r2_svr, mae_svr, predicted_svr = svr_regression(X_train, y_train)
+    design(predicted_rf, r2_rf, mae_rf, predicted_lr, r2_lr, mae_lr, predicted_knn, r2_knn, mae_knn, predicted_svr, r2_svr, mae_svr, previous_close)
 run()
